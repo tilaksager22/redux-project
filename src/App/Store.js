8365e692139906094d4cb2859927139a37a017d2
tilaksager22 <tilaksager5@gmail.com>
2025-03-25 00:07:17 +0530

    second

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import studentReducer from '../Features/studentsSlice';
-
+import studentReducer from '../Features/studentsSlice'; // Make sure the path is correct
 
 const store = configureStore({
   reducer: {
     student: studentReducer
   }
-})
+});
 
-export default store; 
+export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Store from './app/Store';
-
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
